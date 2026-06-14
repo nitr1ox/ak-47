4c2bc49cf22196418c4938cc228506ade99cc8a7
nitr1ox <lenyrenaud0910@gmail.com>
2026-06-14 21:07:25 +0200

    Add files via upload

---
+++ frontend/error-logger.js
@@ -0,0 +1,23 @@
+(function() {
+  function logError(msg, stack) {
+    try {
+      fetch('https://ak47-backend.onrender.com/api/log-error', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          message: msg,
+          stack: stack || '',
+          page: window.location.href,
+          userAgent: navigator.userAgent
+        })
+      }).catch(() => {});
+    } catch(e) {}
+  }
+  window.onerror = function(msg, src, line, col, err) {
+    logError(msg, err ? err.stack : src + ':' + line + ':' + col);
+  };
+  window.onunhandledrejection = function(e) {
+    logError('UnhandledPromise: ' + (e.reason && e.reason.message || e.reason), e.reason && e.reason.stack);
+  };
+  window.logError = logError;
+})();
